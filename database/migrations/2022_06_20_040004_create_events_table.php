d1cf485fb3e00fb904d5bd1a33b54e67db78af1c
paulabhishek <1998vabhishekpaul@gmail.com>
2022-06-20 21:57:29 -0400

    Created new migration files for events, member, admin. Created models for Event, Manage, Member.

--- database/migrations/2022_06_20_040004_create_events_table.php
+++ database/migrations/2022_06_20_040004_create_events_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table-> string('name');
+            $table-> string('location');
+            $table-> string('image');
         });
     }
 
