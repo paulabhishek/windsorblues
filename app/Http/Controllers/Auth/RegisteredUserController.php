baea6d736597c2542df01e2d691d3e0757220836
paulabhishek <1998vabhishekpaul@gmail.com>
2022-07-27 22:25:29 -0400

    Add new field Levels in register view page. also edited the controller to accept the form value.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,7 +45,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'level' => $request->level,
+            'level' => $request->level
         ]);
 
         event(new Registered($user));
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,18 @@
                             </div>
                         </div>
 
+                        <div class="row mb-3">
+                                <label class="col-md-4 col-form-label text-md-end" for="level">{{ __('Select Levels') }}</label>
+                            <div class="col-md-6">
+                                <select name="level" id="level" class="form-control">
+                                    <option selected>Choose...</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                </select>
+                            </div>
+                        </div>
+
+
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
