269a799103eda7d52225fea5b1a9c2c3ba8bcb6b
paulabhishek <1998vabhishekpaul@gmail.com>
2022-08-02 01:51:00 -0400

    1. added user_id to fillable in Event model
    2. added Auth facade in manage/events/create.blade file

--- routes/web.php
+++ routes/web.php
@@ -32,35 +32,15 @@ Route::get('/dashboard', function () {
 require __DIR__.'/auth.php';
 
 
-//Route::resource('manage', ManageController::class);
-
-//Route::get('manage', [ManageController::class, 'index'])->name('manage.index');
-
-//Route::view('membership', 'membership.memberForm')->name('form');
-//Route::post('/send', [MemberController::class, 'sendForm'])->name('send.form');
-//Route::get('members', [MemberController::class, 'index'])->name('member.index');
-
-
-
-
 //EVENT
-//Route::get('event/create', [EventController::class, 'create'])->name('event.create');
-//Route::post('event', [EventController::class, 'store'])->name('event.store');
 Route::get('event', [EventController::class, 'index'])->name('event.index');
 Route::get('event/{id}', [EventController::class, 'show'])->name('event.show');
-//Route::get('event/{id}/edit', [EventController::class, 'edit'])->name('event.edit');
-//Route::patch('event/{id}', [EventController::class, 'update'])->name('event.update');
-//Route::delete('event/{id}', [EventController::class, 'destroy'])->name('event.delete');
+
 
 
 // MEMBER
-//Route::get('member/create', [MemberController::class, 'create'])->name('member.create');
-//Route::post('member', [MemberController::class, 'store'])->name('member.store');
 Route::get('member', [MemberController::class, 'index'])->name('member.index');
-//Route::get('member/{id}', [MemberController::class, 'show'])->name('member.show');
-//Route::get('member/{id}/edit', [MemberController::class, 'edit'])->name('member.edit');
-//Route::patch('member/{id}', [MemberController::class, 'update'])->name('member.update');
-//Route::delete('member/{id}', [MemberController::class, 'destroy'])->name('member.delete');
+
 
 // CONTACT
 Route::get('contact', [ContactController::class, 'index'])->name('contact.index');
@@ -126,6 +106,3 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
