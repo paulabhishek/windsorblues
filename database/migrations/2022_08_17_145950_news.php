acf73837eb03c4bed5eb57fb7a776940ef1fad21
paulabhishek <1998vabhishekpaul@gmail.com>
2022-08-17 11:01:18 -0400

    1. created migration for news DB.

---
+++ database/migrations/2022_08_17_145950_news.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table-> string('name');
+            $table-> string('email');
+            $table-> string('phone');
+            $table-> boolean('newsletter');
+            $table-> boolean('terms');
+            $table->bigInteger('user_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
